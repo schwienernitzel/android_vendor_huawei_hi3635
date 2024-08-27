81a475c876d77e8d748b6cb186b62fdabc4a753e
schwienernitzel <pfelix0803@gmail.com>
2024-08-27 23:38:06 +0000

    Initial commit
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+# LineageOS 13.0 for Huawei hi3635 devices (EMUI 3.1)
+## These vendor blobs were extracted from the following firmware images: 
+
+[`M2-A01L_Firmware_Android_5.1.1_EMUI_3.1_V100R001C100B010`](https://mega.nz/file/rIEGmLZK#Rjy1tcffj35vukEnxFXEyTp-s_08yUOxRvUJ-dV859Q) _(EMUI 3.1)_
+######
+[`M2-801W_Firmware_Android_6.0.1_EMUI4.0_V100R001C232B202`](https://mega.nz/file/PI8AHQCD#h3_yWoiff0jRiRvBS_eYiyKDysOvgXsvlX18augJ0DA) _(EMUI 4)_
+
+> **Note:** These proprietary blobs were selected from various firmware versions in order to get the ROM to work in the best way possible for our devices. The origin of each library is marked as either _EMUI 3.1_ or _EMUI 4_ inside the `vendor.mk` as it is also stated above.
+
+## This vendor tree is intended to be used with these repositories: 
+
+[`android_device_huawei_hi3635`](https://github.com/schwienernitzel/android_device_huawei_hi3635) _(branch: cm-13.0)_
+######
+[`android_kernel_huawei_hi3635`](https://github.com/schwienernitzel/android_kernel_huawei_hi3635) _(branch: cm-13.0)_
+
+> **Note:** You can find a step-by-step tutorial on _how to build_ inside the `README.md` of the device tree. Please note that this work is opensource and every kind of **contribution is highly appreciated.** Feel free to create a fork, pull request or issue to make your contribution. Please let me know if there are code mistakes, typos, or outdated information. If you like my work, [buy me a cup of coffee.](https://paypal.me/felixpat)
---
+++ vendor.mk
@@ -0,0 +1,51 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+VENDOR_PATH := vendor/huawei/hi3635/proprietary
+
+# Oeminfo & tee blobs (EMUI4)
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/lib/lib_nve_operation.so:/system/lib/lib_nve_operation.so \
+    $(VENDOR_PATH)/lib/lib_oeminfo_interface.so:/system/lib/lib_oeminfo_interface.so \
+    $(VENDOR_PATH)/lib/libnvme.so:/system/lib/libnvme.so \
+    $(VENDOR_PATH)/lib/liboeminfo.so:/system/lib/liboeminfo.so \
+    $(VENDOR_PATH)/lib/liboeminfo_jni.so:/system/lib/liboeminfo_jni.so \
+    $(VENDOR_PATH)/lib/libteec.so:/system/lib/libteec.so \
+    $(VENDOR_PATH)/lib/libaes.so:/system/lib/libaes.so \
+    $(VENDOR_PATH)/lib64/lib_nve_operation.so:/system/lib64/lib_nve_operation.so \
+    $(VENDOR_PATH)/lib64/lib_oeminfo_interface.so:/system/lib64/lib_oeminfo_interface.so \
+    $(VENDOR_PATH)/lib64/libnvme.so:/system/lib64/libnvme.so \
+    $(VENDOR_PATH)/lib64/liboeminfo.so:/system/lib64/liboeminfo.so \
+    $(VENDOR_PATH)/lib64/liboeminfo_jni.so:/system/lib64/liboeminfo_jni.so \
+    $(VENDOR_PATH)/lib64/libteec.so:/system/lib64/libteec.so \
+    $(VENDOR_PATH)/lib64/libaes.so:/system/lib64/libaes.so
+
+# Graphic blobs (EMUI4)
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/lib/hw/copybit.hi3635.so:/system/lib/hw/copybit.hi3635.so \
+    $(VENDOR_PATH)/lib/hw/gralloc.hi3635.so:/system/lib/hw/gralloc.hi3635.so \
+    $(VENDOR_PATH)/lib/hw/memtrack.hi3635.so:/system/lib/hw/memtrack.hi3635.so \
+    $(VENDOR_PATH)/lib/libion.so:/system/lib/libion.so \
+    $(VENDOR_PATH)/vendor/lib/egl/libGLES_mali.so:/system/lib/egl/libGLES_mali.so \
+    $(VENDOR_PATH)/lib64/hw/copybit.hi3635.so:/system/lib64/hw/copybit.hi3635.so \
+    $(VENDOR_PATH)/lib64/hw/gralloc.hi3635.so:/system/lib64/hw/gralloc.hi3635.so \
+    $(VENDOR_PATH)/lib64/hw/memtrack.hi3635.so:/system/lib64/hw/memtrack.hi3635.so \
+    $(VENDOR_PATH)/lib64/libion.so:/system/lib64/libion.so \
+    $(VENDOR_PATH)/vendor/lib64/egl/libGLES_mali.so:/system/lib64/egl/libGLES_mali.so
+
+# Light blobs (EMUI4)
+PRODUCT_COPY_FILES += \
+    $(VENDOR_PATH)/lib/hw/lights.default.so:/system/lib/hw/lights.default.so \
+    $(VENDOR_PATH)/lib64/hw/lights.default.so:/system/lib64/hw/lights.default.so
